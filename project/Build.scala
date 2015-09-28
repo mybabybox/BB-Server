0ba12bf280e472d7f288e9c5b4f9bd599ed1adae
keithlei01 <keithlei01@gmail.com>
2015-09-28 22:24:47 +0800

    Dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -17,7 +17,6 @@ object ApplicationBuild extends Build {
     "mysql" % "mysql-connector-java" % "5.1.36",
     "org.hibernate" % "hibernate-entitymanager" % "4.2.7.Final",
     "org.hamcrest" % "hamcrest-all" % "1.3",
-    "commons-io" % "commons-io" % "2.1",
     "joda-time" % "joda-time" % "2.3",
     "com.google.guava" % "guava" % "12.0",
     "be.objectify"  %%  "deadbolt-java"     % "2.1-RC2",
@@ -26,11 +25,12 @@ object ApplicationBuild extends Build {
     "org.apache.httpcomponents" % "httpclient" % "4.2.5",
     "net.coobird" % "thumbnailator" % "0.4.7",
     "com.clever-age" % "play2-elasticsearch" % "0.5.5",
-    "commons-pool" % "commons-pool" % "1.5.5",
+    "commons-pool" % "commons-pool" % "1.6",
     "commons-lang" % "commons-lang" % "2.6",
     "commons-collections" % "commons-collections" % "3.2.1",
+    "commons-io" % "commons-io" % "2.4",
     "biz.source_code" % "base64coder" % "2010-12-19",
-    "redis.clients" % "jedis" % "2.7.3",
+    "redis.clients" % "jedis" % "2.2.1",
     "org.sedis" % "sedis" % "1.0.1",
     "org.jsoup" % "jsoup" % "1.7.3"
   )
