6a81ad34ae39a049f8dd92a8283633dc887d94d3
keithlei01 <keithlei01@gmail.com>
2015-10-30 23:41:54 +0800

    ga

--- public/app/scripts/tracking.js
+++ public/app/scripts/tracking.js
@@ -8,8 +8,8 @@ var ga_send_pv = function(page_code) {
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    
-    ga('create', 'UA-53905621-1', 'auto');
+
+    ga('create', 'UA-69507450-2', 'auto');
     ga('send', 'pageview');
     console.log('ga_send_pv - '+window.location.host);
 }
